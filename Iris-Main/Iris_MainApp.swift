3dae0d616fce16540827ba7d48a88c7b7825b317
arrana16 <69128309+arrana16@users.noreply.github.com>
2026-03-13 16:59:55 -0400

    Initial commit

---
+++ Iris-Main/Iris_MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Iris_MainApp.swift
+//  Iris-Main
+//
+//  Created by Abdur-Rahman Rana on 2026-03-13.
+//
+
+import SwiftUI
+
+@main
+struct Iris_MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Iris-MainTests/Iris_MainTests.swift
@@ -0,0 +1,17 @@
+//
+//  Iris_MainTests.swift
+//  Iris-MainTests
+//
+//  Created by Abdur-Rahman Rana on 2026-03-13.
+//
+
+import Testing
+@testable import Iris_Main
+
+struct Iris_MainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
